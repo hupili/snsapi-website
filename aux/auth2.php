9a27d588e875b4a53fcd158a9428f957908e790e
hupili <hpl1989@gmail.com>
2012-11-07 13:37:59 +0800

    add '/' to solve renren redirection

--- aux/auth2.php
+++ aux/auth2.php
@@ -1,7 +1,7 @@
 <?php
 
 header("Status: 301 Moved Permanently");
-header("Location:http://127.0.0.1:8080/auth/second/". $_SERVER['QUERY_STRING']);
+header("Location:http://127.0.0.1:8080/auth/second/?". $_SERVER['QUERY_STRING']);
 exit;
 
 ?>
